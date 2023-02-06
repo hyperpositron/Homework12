028264c0b233024c0e7437c2bf3f88e1ef3da537
Александр <hyperpositron@gmail.com>
2023-02-06 14:56:16 +0200

    create class author and book!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world2!");
+        Author lermontov = new Author("Михаил", "Лермонтов");
+        Author tolstoy = new Author("Лев", "Толстой");
+
+        Book mtsyri = new Book("Мцыри", 1839, lermontov);
+        Book warAndPeace = new Book("Война и мир", 1867, tolstoy);
+        System.out.println(mtsyri);
+        System.out.println(warAndPeace);
     }
 }
